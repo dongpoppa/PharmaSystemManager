61c58de211ac65663ed0049f57353692c38a5608
longdh92 <54471003+longdh92@users.noreply.github.com>
2020-03-16 20:37:42 +0700

    package DAO, model
    Employee, EmployeeDAO, update EmployeeJInternalFrame

---
+++ PharmaSystemManager/src/DAO/EmployeeDAO.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import helper.JdbcHelper;
+import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Employee;
+
+/**
+ *
+ * @author Long
+ */
+public class EmployeeDAO {
+
+    public void insert(Employee model) {
+        String sql = "INSERT INTO NhanVien (MaNV, MatKhau, VaiTro, HoVaTen, NgaySinh, NgayLamViec, SDTNV, EmailNV, DiaChiNV, TrangThaiNV, MaDaiLy) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        JdbcHelper.executeUpdate(sql, model.getEmployeeID(), model.getPassword(), model.isRole(), model.getName(), model.getDateOfBirth(),
+                model.getStartDate(), model.getPhone(), model.getEmail(), model.getAddress(), model.getStatus(), model.getStoreID());
+    }
+
+    public void update(Employee model) {
+        String sql = "UPDATE NhanVien SET MatKhau = ?, VaiTro = ?, HoVaTen = ?, NgaySinh = ?, NgayLamViec = ?, SDTNV = ?, EmailNV = ?, DiaChiNV = ?, TrangThaiNV = ?, MaDaiLy = ? WHERE MaNV = ?";
+        JdbcHelper.executeUpdate(sql, model.getPassword(), model.isRole(), model.getName(), model.getDateOfBirth(),
+                model.getStartDate(), model.getPhone(), model.getEmail(), model.getAddress(), model.getStatus(), model.getStoreID(), model.getEmployeeID());
+    }
+
+    public void delete(String MaNV) {
+        String sql = "DELETE FROM NhanVien WHERE MaNV = ?";
+        JdbcHelper.executeUpdate(sql, MaNV);
+    }
+
+    public List<Employee> select() {
+        String sql = "SELECT * FROM NhanVien";
+        return select(sql);
+    }
+
+    public Employee findById(String manv) {
+        String sql = "SELECT * FROM NhanVien WHERE MaNV = ?";
+        List<Employee> list = select(sql, manv);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+
+    private List<Employee> select(String sql, Object... args) {
+        List<Employee> list = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = JdbcHelper.executeQuery(sql, args);
+                while (rs.next()) {
+                    Employee model = readFromResultSet(rs);
+                    list.add(model);
+                }
+            } finally {
+                rs.getStatement().getConnection().close();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+
+    private Employee readFromResultSet(ResultSet rs) throws SQLException {
+        Employee model = new Employee();
+        model.setEmployeeID(rs.getString("MaNV"));
+        model.setPassword(rs.getString("MatKhau"));
+        model.setRole(rs.getBoolean("VaiTro"));
+        model.setName(rs.getString("HoVaTen"));
+        model.setDateOfBirth(rs.getDate("NgaySinh"));
+        model.setStartDate(rs.getDate("NgayLamViec"));
+        model.setPhone(rs.getString("SDTNV"));
+        model.setEmail(rs.getString("EmailNV"));
+        model.setAddress(rs.getString("DiaChiNV"));
+        model.setStatus(rs.getString("TrangThaiNV"));
+        model.setStoreID(rs.getString("MaDaiLy"));
+        return model;
+    }
+}
