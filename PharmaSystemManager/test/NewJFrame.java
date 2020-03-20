b1d0f40099d2ff946395fa68ead7374492bd4c02
DongPoppa <lamnhungcailinhtinh@gmail.com>
2020-03-21 00:55:19 +0700

    print libary

--- PharmaSystemManager/test/NewJFrame.java
+++ PharmaSystemManager/test/NewJFrame.java
@@ -1,12 +1,25 @@
 
-import com.sun.glass.events.KeyEvent;
-import java.util.Objects;
-import javafx.beans.value.ChangeListener;
-import javax.swing.SwingUtilities;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.JTextComponent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.data.JRTableModelDataSource;
+import net.sf.jasperreports.engine.design.JRDesignQuery;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -24,12 +37,9 @@ public class NewJFrame extends javax.swing.JFrame {
      */
     public NewJFrame() {
         initComponents();
-        jlist.setVisible(false);
-        jtablesc.setVisible(false);
         setSize(1000, 700);
         setLocationRelativeTo(null);
-        aasd();
-
+        
     }
 
     /**
@@ -41,107 +51,150 @@ public class NewJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jtablesc = new javax.swing.JScrollPane();
-        jlist = new javax.swing.JList<>();
-        txt = new javax.swing.JTextField();
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jtablesc123 = new javax.swing.JScrollPane();
-        jTable2 = new javax.swing.JTable();
+        tblGridView = new javax.swing.JTable();
+        txt1 = new javax.swing.JTextField();
+        txt2 = new javax.swing.JTextField();
+        txt3 = new javax.swing.JTextField();
+        rdoTien = new javax.swing.JRadioButton();
+        rdoThe = new javax.swing.JRadioButton();
+        cbx1 = new javax.swing.JComboBox<>();
+        btnPrint = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jsfQuantity = new com.toedter.components.JSpinField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        jlist.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jlist.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jlistMouseClicked(evt);
-            }
-        });
-        jtablesc.setViewportView(jlist);
-
-        getContentPane().add(jtablesc);
-        jtablesc.setBounds(70, 150, 240, 220);
-
-        txt.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                txtKeyPressed(evt);
-            }
-        });
-        getContentPane().add(txt);
-        txt.setBounds(70, 110, 240, 40);
-
-        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+        tblGridView.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {"Dong", "1999", "Nam", "DZ"},
+                {"Quan", "1991", "KXD", "(n)"},
+                {"Long", "1992", "KXD", "(y)"}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "1", "2", "3", "4"
             }
         ));
-        jtablesc123.setViewportView(jTable2);
+        jtablesc123.setViewportView(tblGridView);
 
         getContentPane().add(jtablesc123);
-        jtablesc123.setBounds(70, 170, 630, 380);
+        jtablesc123.setBounds(70, 310, 720, 210);
+        getContentPane().add(txt1);
+        txt1.setBounds(210, 90, 160, 24);
+        getContentPane().add(txt2);
+        txt2.setBounds(210, 130, 160, 24);
+        getContentPane().add(txt3);
+        txt3.setBounds(210, 170, 160, 24);
+
+        buttonGroup1.add(rdoTien);
+        rdoTien.setSelected(true);
+        rdoTien.setText("Tiền");
+        getContentPane().add(rdoTien);
+        rdoTien.setBounds(420, 210, 56, 28);
+
+        buttonGroup1.add(rdoThe);
+        rdoThe.setText("Thẻ");
+        getContentPane().add(rdoThe);
+        rdoThe.setBounds(560, 210, 53, 28);
+
+        cbx1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Dong DZ", "Quan XZ", "Long XZ", "Dai XZ", "Hiep XZ" }));
+        getContentPane().add(cbx1);
+        cbx1.setBounds(420, 140, 270, 26);
+
+        btnPrint.setText("Print");
+        btnPrint.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPrintActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnPrint);
+        btnPrint.setBounds(400, 570, 57, 32);
+
+        jLabel1.setText("ID");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(170, 90, 11, 16);
+
+        jLabel2.setText("Thanh Tien");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(140, 180, 80, 16);
+
+        jLabel3.setText("Ten");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(170, 140, 21, 16);
+
+        jLabel4.setText("Quantity");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(130, 220, 90, 16);
+        getContentPane().add(jsfQuantity);
+        jsfQuantity.setBounds(210, 210, 160, 24);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jlistMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jlistMouseClicked
+    private void btnPrintActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPrintActionPerformed
+
         // TODO add your handling code here:
-        txt.setText(jlist.getSelectedValue());
-        jlist.setVisible(false);
-        jtablesc.setVisible(false);
-
-    }//GEN-LAST:event_jlistMouseClicked
-
-    private void txtKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtKeyPressed
-        // TODO add your handling code here:
-        if (evt.getKeyCode() == KeyEvent.VK_BACKSPACE) {
-            jlist.setVisible(false);
-            jtablesc.setVisible(false);
-        }
-        if (evt.getKeyCode() == KeyEvent.VK_X) {
-            jlist.setVisible(false);
-            jtablesc.setVisible(false);
-        }
-    }//GEN-LAST:event_txtKeyPressed
-    protected void updateFieldState() {
-        if (txt.getText() != null || txt.getText().trim().length() > 0) {
-            jlist.setVisible(true);
-            jtablesc.setVisible(true);
-        } else {
-            jlist.setVisible(false);
-            jtablesc.setVisible(false);
-        }
-    }
-
-    void aasd() {
-        DocumentListener d1 = new DocumentListener() {
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                updateFieldState();
+        try {
+            JasperDesign jasdi = JRXmlLoader.load("E:\\OneDrive - Nguyen Sieu School\\Documents\\GitHub\\PharmaSystemManager\\PharmaSystemManager\\test\\testBill.jrxml");
+//            String sql = "SELECT `bill_no`, `item_name`, `quantity`, `price` FROM `item` WHERE bill_no='" + txtbillnumber.getText() + "'";
+//            JRDesignQuery newQuery = new JRDesignQuery();
+//            newQuery.setText(sql);
+//
+//            jasdi.setQuery(newQuery);
+            /////////////
+            HashMap<String, Object> para = new HashMap<>();
+            para.put("1", txt1.getText());
+            para.put("2", txt2.getText());
+            para.put("3", txt3.getText());
+            para.put("4", String.valueOf(jsfQuantity.getValue()));
+            para.put("5", cbx1.getSelectedItem().toString());
+            if (rdoTien.isSelected()) {
+                para.put("6", "Tiền");
+            } else {
+                para.put("6", "Thẻ");
             }
+            DefaultTableModel tableModel=(DefaultTableModel)tblGridView.getModel();
+            JasperReport js = JasperCompileManager.compileReport(jasdi);
+            JasperPrint jp = JasperFillManager.fillReport(js, para,  new JRTableModelDataSource(tableModel));
+            JasperViewer.viewReport(jp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(rootPane, e);
+        }
+        
 
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                updateFieldState();
-            }
+    }//GEN-LAST:event_btnPrintActionPerformed
 
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                updateFieldState();
-            }
-
-        };
-        txt.getDocument().addDocumentListener(d1);
-    }
-
+//    String fileName = "C:\\Documents and Settings\\abhijit\\JasperSimpleReport.jasper";
+//    String outFileNamePDF = "c:\\JasperSimpleReport.pdf";
+//    HashMap hm = new HashMap();
+//
+//    public void generateReport() throws ClassNotFoundException, InstantiationException, IllegalAccessException, JRException {
+//        try {
+//
+//            System.out.println("Filling report...");
+//            hm.put("", "Hello Report");
+//            //JasperReport manager=JasperManager.compileReport(fileName);
+//            JasperPrint print = JasperFillManager.fillReportToFile(fileName, outFileNamePDF, hm);
+//            JRExporter exporter = new net.sf.jasperreports.engine.export.JRPdfExporter();
+//            //parameter used for the destined file.
+//            exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME,
+//                    outFileNamePDF);
+//            exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+//            //export to .pdf
+//            exporter.exportReport();
+//            System.out.println("Created file: " + outFileNamePDF);
+//            System.out.println("Done!");
+//
+//        } catch (JRException e) {
+//            e.printStackTrace();
+//        }
+//    }
     /**
      * @param args the command line arguments
      */
@@ -156,21 +209,21 @@ public class NewJFrame extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
-
+                    
                 }
             }
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(NewJFrame.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
+            
         } catch (InstantiationException ex) {
             java.util.logging.Logger.getLogger(NewJFrame.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
+            
         } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(NewJFrame.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
+            
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(NewJFrame.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
@@ -186,10 +239,20 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTable jTable2;
-    private javax.swing.JList<String> jlist;
-    private javax.swing.JScrollPane jtablesc;
+    private javax.swing.JButton btnPrint;
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JComboBox<String> cbx1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private com.toedter.components.JSpinField jsfQuantity;
     private javax.swing.JScrollPane jtablesc123;
-    private javax.swing.JTextField txt;
+    private javax.swing.JRadioButton rdoThe;
+    private javax.swing.JRadioButton rdoTien;
+    private javax.swing.JTable tblGridView;
+    private javax.swing.JTextField txt1;
+    private javax.swing.JTextField txt2;
+    private javax.swing.JTextField txt3;
     // End of variables declaration//GEN-END:variables
 }
