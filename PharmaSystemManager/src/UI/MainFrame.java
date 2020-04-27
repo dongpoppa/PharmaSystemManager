ecc71f9fbae35124a1b6a66c050ebbb02fe1dce3
longdh92 <54471003+longdh92@users.noreply.github.com>
2020-04-27 08:46:34 +0700

    Update MainFrame.java

--- PharmaSystemManager/src/UI/MainFrame.java
+++ PharmaSystemManager/src/UI/MainFrame.java
@@ -761,6 +761,10 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void openReport(int pnl) {
         closeFrames();
+        if (!ShareHelper.USER.isRole()) {
+            DialogHelper.alert(this, "You can't access here");
+            return;
+        }
         StatisticJInternalFrame statisticJInternalFrame = new StatisticJInternalFrame(this, pnl);
         addFrame(statisticJInternalFrame);
     }
